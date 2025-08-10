941eebb966690dccc742fc4eb27200e13e4b1c30
mzuenni <michi.zuendorf@gmail.com>
2025-08-10 23:18:57 +0200

    merge

--- gcpc2022/formulaflatland/input_validators/validator/input_validator.cpp
+++ gcpc2022/formulaflatland/input_validators/validator/input_validator.cpp
@@ -1,8 +1,4 @@
 #include "validate.h"
-#include "boost/graph/adjacency_list.hpp"
-#include "boost/graph/planar_face_traversal.hpp"
-#include "boost/graph/boyer_myrvold_planar_test.hpp"
-#include "is_straight_line_drawing.hpp"
 using namespace std;
 
 using namespace ctd;
@@ -10,22 +6,9 @@ using namespace ctd;
 #define all(x) begin(x), end(x)
 #define sz(x) (Integer)(x).size()
 
-using adj = boost::adjacency_list<boost::vecS,
-								  boost::vecS,
-								  boost::undirectedS,
-								  boost::property<boost::vertex_index_t, Integer>,
-								  boost::property<boost::edge_index_t, Integer>
-								 >;
-using vec_t = vector<boost::graph_traits<adj>::edge_descriptor>;
-
 struct coord_t {
   Integer x, y;
 };
-
-using straight_line_drawing_storage_t = std::vector<coord_t>;
-using straight_line_drawing_t = boost::iterator_property_map<straight_line_drawing_storage_t::iterator, 
-															 boost::property_map<adj, boost::vertex_index_t>::type
-															>;
 
 int main(int argc, char **argv) {
 	using namespace InputValidator;
@@ -44,8 +27,6 @@ int main(int argc, char **argv) {
 		seenPos.insert({x, y});
 	}
 
-
-	adj g(n);
 	vector<Integer> deg(n);
 	vector<pair<Integer, Integer>> edgeList;
 	for (Integer i = 0; i < m; i++) {
@@ -54,7 +35,6 @@ int main(int argc, char **argv) {
 		if (a == b) juryOut << "Selfloop: " << a << "<->" << a << WA;
 		a--;
 		b--;
-		add_edge(a, b, g);
 		deg[a]++;
 		deg[b]++;
 		if (a < b) swap(a, b);
@@ -69,25 +49,10 @@ int main(int argc, char **argv) {
 		if (deg[i] <= 2) juryOut << "Min-degree too small: " << i+1 << WA;
 	}
 
-	//Initialize the interior edge index
-	using namespace boost;
-	property_map<adj, edge_index_t>::type e_index = get(edge_index, g);
-	graph_traits<adj>::edges_size_type edge_count = 0;
-	graph_traits<adj>::edge_iterator ei, ei_end;
-	for(tie(ei, ei_end) = edges(g); ei != ei_end; ++ei) {
- 		put(e_index, *ei, edge_count++);
-	}
+	// check graph planar
+	// check graph straight line
+	// this was done offline with boost
 
-	vector<vec_t> embedding(n);
-	if (!boyer_myrvold_planarity_test(boyer_myrvold_params::graph = g,
-									  boyer_myrvold_params::embedding = &embedding[0])) {
-		juryOut << "Graph not planar!" << WA;
-	}
-
-	straight_line_drawing_t straight_line_drawing(pos.begin(), get(vertex_index, g));
-	if (!is_straight_line_drawing(g, straight_line_drawing)) {
-		juryOut << "Embedding is not straight line!" << WA;
-	}
 	return AC;
 
 }
