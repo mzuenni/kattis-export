be8def9226d466df9ebcdd8f4c81aac4ca43d96a
Arnar Bjarni Arnarson <arnar.b.arnarson@gmail.com>
2026-07-07 15:45:18 +0000

    [gcpc2017] uberwatch updated

--- gcpc2017/uberwatch/submissions/accepted/uberwatch_moritz.py
+++ gcpc2017/uberwatch/submissions/accepted/uberwatch_moritz.py
@@ -4,11 +4,10 @@ Created on 18.05.2017
 @author: Moritz Fuchs
 '''
 
-n,m = map(int,raw_input().split())
-x = list(map(int,raw_input().split()))
+n,m = list(map(int,input().split()))
+x = list(map(int,input().split()))
 
 dp = [0]*n
 for i in range(m,n):
     dp[i] = max(dp[i-1], dp[i-m] + x[i])
 print(dp[n-1])
-
